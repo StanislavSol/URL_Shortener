963a67333bf9eb7692e8b456c546a8daee9dbd32
имя фамилия < >
2024-02-20 18:28:13 +0300

    fix

--- database.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS urls;
-
-CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    target_url varchar(255),
-    key varchar(255),
-    url varchar(255) UNIQUE NOT NULL,
-    created_at DATE DEFAULT CURRENT_TIMESTAMP
-);
